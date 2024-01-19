2f8d50d94f9d846799cc89417668864f8aa6b911
DSHRX <dcs630@mun.ca>
2024-01-19 14:33:33 -0500

    Updated the POM with the slf4j version and have the JavaGrepImp overrides put in

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.io.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.log4j.BasicConfigurator;
+
 
 public class JavaGrepImp implements JavaGrep {
 
@@ -32,6 +34,43 @@ public class JavaGrepImp implements JavaGrep {
             javaGrepImp.logger.error("Error: Unable to process", ex);
         }
     }
+
+    @Override 
+    public void process() throws IOException {
+        //TODO actual logic
+    }
+
+    @Override
+    public List<File> listFiles(String rootDir) {
+        //TODO actual logic
+        return new List<File>();
+            
+    }
+
+    @Override
+    public List<String> readLines(File inputFile) {
+        //TODO actual logic
+        List<String> myList = new List<String>();
+        return myList;
+
+    }
+
+    @Override
+    public boolean containsPattern(String line) {
+        //TODO real logic
+        return false;
+    }
+
+    @Override
+    public void writeToFile(List<String> lines) throws IOException{
+        //TODO real logic
+    }
+
+    @Override
+    public String getRootPath() { return rootPath; }
+
+    @Override 
+    public void setRootPath(String rootPath) { this.rootPath = rootPath;}
 
     @Override
     public String getRegex() { return regex; }
