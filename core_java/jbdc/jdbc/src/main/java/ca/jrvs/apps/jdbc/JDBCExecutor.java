465d150b889c8a4e2430f7f9c10807a23c739fbc
DSHRX <dcs630@mun.ca>
2024-02-14 10:40:03 -0500

    Completed the course, updated JDBCExecutor and added created the DataAccessObject, DataTransferObject and added Customer and CustomerDAO

--- core_java/jbdc/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jbdc/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -8,15 +8,30 @@ import java.sql.Statement;
 public class JDBCExecutor {
     
     public static void main(String... args) {
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "password");
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "GresPassPost");
         try{
             Connection connection = dcm.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
-            while ((resultSet.next())) {
-                System.out.println(resultSet.getInt(1));
-            }
-        }catch(SQLException e){
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            Customer customer = new Customer();
+            customer.setFirstName("John");
+            customer.setLastName("Adams");
+            customer.setEmail("jadams.wh.go");
+            customer.setAddress("1234 Main St");
+            customer.setCity("Arlington");
+            customer.setState("VA");
+            customer.setPhone("(555) 555-9845");
+            customer.setZipCode("01234");
+
+            Customer dbCustomer = customerDAO.create(customer);
+            System.out.println(dbCustomer);
+            dbCustomer = customerDAO.findById(dbCustomer.getId());
+            System.out.println(dbCustomer);
+            dbCustomer.setEmail("john.adams@wh.gov");
+            dbCustomer = customerDAO.update(dbCustomer);
+            System.out.println(dbCustomer);
+            customerDAO.delete(dbCustomer.getId());
+
+        }catch(SQLException e) {
             e.printStackTrace();
         }
     }
