dddd504a66ccf0777e100719a5cdb1ce6eae7609
DSHRX <dcs630@mun.ca>
2024-02-14 16:17:27 -0500

    Created AVRestAPO, JsonParser and added the Company, Dividend and Financial files Updated pom

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/example/dto/AVRestAPI.java
@@ -0,0 +1,37 @@
+package ca.jrvs.apps.twitter.example.dto;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonParser;
+
+public class AVRestAPI {
+
+    public static void main(String... args) {
+    String symbol = "MSFT";
+    String apiKey = "BLANK_FOR_SAFETY";
+    HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol="+symbol+"&datatype=json"))
+				.header("X-RapidAPI-Key", apiKey)
+				.header("X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com")
+				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+    try {
+	    HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+	    System.out.println(response.body());
+    } catch (InterruptedException e) {
+	    e.printStackTrace();
+    } catch (JsonMappingException e) {
+	    e.printStackTrace();
+    } catch (JsonProcessingException e) {
+	    e.printStackTrace();
+    } catch (IOException e) {
+	    e.printStackTrace();
+    }
+    //System.out.println(response.body());
+    }
+}
